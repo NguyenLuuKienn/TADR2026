fecb896f17771fd65b82b97f58e1297abcb89404
nguyenluukienn <nguyenluukienn.d23ctc4@muce.edu.vn>
2026-05-06 20:10:44 +0700

    Force light mode on all devices - prevent dark mode interference

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  // Disable dark mode support
+  darkMode: false,
+}
